27998516f9ba7e2d2e8b37d43bf4a5d4cc71795d
atiwari3bu <atiwari3@binghamton.edu>
2019-01-01 11:51:02 +0530

    Day 15: Linked List

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-program_name=day14.cpp
+program_name=day15.cpp
 
 all:
 	g++ -std=c++14 $(program_name)
---
+++ day15.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstddef>
+using namespace std;	
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+
+      Node* insert(Node *head,int data)
+      {
+          //Complete this method
+          Node* created_node=new Node(data);
+          Node* traverse=head;
+          if(traverse==nullptr){
+               head=created_node;
+          }
+          else{
+              while(traverse->next!=nullptr){
+                  traverse=traverse->next;
+              }
+              traverse->next=created_node;
+          }
+          return head;
+      }
+
+      void display(Node *head)
+      {
+          Node *start=head;
+          while(start)
+          {
+              cout<<start->data<<" ";
+              start=start->next;
+          }
+      }
+};
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+	mylist.display(head);
+		
+}
