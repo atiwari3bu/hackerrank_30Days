4f832cf6067307f3672ee6a9d3d8114e9b956e48
atiwari3bu <atiwari3@binghamton.edu>
2019-01-03 12:10:20 +0530

    Day 23: BST Level-Order Traversal

---
+++ day23.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <cstddef>
+#include <queue>
+#include <string>
+#include <cstdlib>
+
+using namespace std;	
+class Node{
+    public:
+        int data;
+        Node *left,*right;
+        Node(int d){
+            data=d;
+            left=right=NULL;
+        }
+};
+class Solution{
+    public:
+  		Node* insert(Node* root, int data){
+            if(root==NULL){
+                return new Node(data);
+            }
+            else{
+                Node* cur;
+                if(data<=root->data){
+                    cur=insert(root->left,data);
+                    root->left=cur;
+                }
+                else{
+                   cur=insert(root->right,data);
+                   root->right=cur;
+                 }           
+           return root;
+           }
+        }
+  
+/* Compute the "height" of a tree -- the number of 
+    nodes along the longest path from the root node 
+    down to the farthest leaf node.*/
+int height(Node* node) 
+{ 
+    if (node==NULL) 
+        return 0; 
+    else
+    { 
+        /* compute the height of each subtree */
+        int lheight = height(node->left); 
+        int rheight = height(node->right); 
+  
+        /* use the larger one */
+        if (lheight > rheight) 
+            return(lheight+1); 
+        else return(rheight+1); 
+    } 
+}
+
+
+    void printLevelOrder(Node* root) 
+{ 
+    int h = height(root); 
+    int i; 
+    for (i=1; i<=h; i++) 
+        printGivenLevel(root, i); 
+} 
+  
+/* Print nodes at a given level */
+void printGivenLevel(Node* root, int level) 
+{ 
+    if (root == NULL) 
+        return; 
+    if (level == 1) 
+        printf("%d ", root->data); 
+    else if (level > 1) 
+    { 
+        printGivenLevel(root->left, level-1); 
+        printGivenLevel(root->right, level-1); 
+    } 
+} 
+
+void levelOrder(Node * root){
+      //Write your code here
+      printLevelOrder(root);
+    
+  
+    } 
+
+};//End of Solution
+int main(){
+    Solution myTree;
+    Node* root=NULL;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        root= myTree.insert(root,data);
+    }
+    myTree.levelOrder(root);
+    return 0;
+}
